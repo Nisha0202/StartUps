a8862cc701c3bf3bfbe594dbcddd8e26aab5a77d
Nisha0202 <nishajabatunnessa@gmail.com>
2024-11-13 01:56:35 +0600

    Prisma schema + Next auth

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,19 @@
-// auth.ts or another service file
+import { PrismaClient } from '@prisma/client'
 
-import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient()
 
-const prisma = new PrismaClient();
-
-export async function createUser(name: string, image: string, githubId: string) {
-  const user = await prisma.user.create({
-    data: {
-      name,
-      image,
- 
-    },
-  });
-  return user;
+async function main() {
+  // ... you will write your Prisma Client queries here
 }
 
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
 
 
